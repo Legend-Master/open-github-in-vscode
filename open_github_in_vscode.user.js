45cf0edd98330bd14ac849284508d3386c4c925c
Tony <legendmastertony@gmail.com>
2023-01-30 18:02:21 +0800

    Bump version from 0.0.2 to 0.0.3

--- open_github_in_vscode.user.js
+++ open_github_in_vscode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GitHub in VSCode desktop
 // @namespace   https://github.com/Legend-Master
-// @version     0.0.2
+// @version     0.0.3
 // @author      Tony
 // @description A userscript that adds a button to open a repo in VSCode desktop using Remote Repositories and GitHub Repositories extensions
 // @icon        https://github.githubassets.com/pinned-octocat.svg

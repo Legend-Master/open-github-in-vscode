2f88cff2869767111cfcc80f21535b3b17e7d90a
Tony <legendmastertony@gmail.com>
2024-07-31 09:00:45 +0800

    Fix for new class name

--- open_github_in_vscode.user.js
+++ open_github_in_vscode.user.js
@@ -80,7 +80,7 @@
 
 		vscodeButton = document.createElement('a')
 		// vscodeButton.className = siblingElement.className
-		vscodeButton.className = 'types__StyledButton-sc-ws60qy-0 gsAScr'
+		vscodeButton.className = 'types__StyledButton-sc-ws60qy-0 feqCqy'
 		vscodeButton.href = `vscode://ms-vscode.remote-repositories/open?url=${window.location}`
 		vscodeButton.innerHTML = `${vscodeLogo} Open in vscode`
 		siblingElement.before(vscodeButton)
